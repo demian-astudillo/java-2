0d4b31e425a0e6c38ea31e720fba4064f0ba6e9c
demian-astudillo <dem.astudillo@gmail.com>
2025-01-08 22:05:54 -0300

    v2

--- src/com/cato/main/Main.java
+++ src/com/cato/main/Main.java
@@ -1,7 +1,24 @@
 package com.cato.main;
 
+import com.cato.musica.musica;
+import com.cato.musica.podcasts;
+
 public class Main {
     public static void main(String[] args) {
+        musica miMusica = new musica();
+
+        miMusica.setNombre("Lunatique");
+        miMusica.setAlbum("Triforce");
+
+        podcasts miPodcast = new podcasts();
+
+        miPodcast.setNombre("Dibujando");
+        miPodcast.setDescripcion("pq no me sale aghhhh");
+
+        System.out.println(miPodcast.getNombre());
+        System.out.println(miMusica.getNombre());
+        System.out.println(miMusica.getAlbum());
+
 
     }
 }
